66ea0c756c871f68779fc68e159d30083b65495f
khaleeq-dsc <109784524+khaleeq-dsc@users.noreply.github.com>
2022-10-26 18:53:29 +0500

    Try Basic TS types

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,16 @@
 "use strict";
+/* Basic Types */
+let id = 5;
+console.log('id of type number', id);
+let userName = 'Khaleeq';
+console.log('userName of type string', userName);
+let isPresent = true;
+isPresent = false;
+console.log('isPresent of type boolean', isPresent);
+let x = 'DynaSoft';
+x = 1234;
+console.log('x of type any', x);
+let idsList = [1, 2, 3, 5];
+console.log('idsList of type number array', idsList);
+let namesList = ['khaleeq', { 'name': 'Nouman' }];
+console.log('namesList of type any array', namesList);
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+/* Basic Types */
+
+let id: number = 5
+console.log('id of type number', id)
+
+let userName:string = 'Khaleeq'
+console.log('userName of type string', userName)
+
+let isPresent : boolean = true
+isPresent = false
+console.log('isPresent of type boolean', isPresent)
+
+let x : any = 'DynaSoft'
+x = 1234
+console.log('x of type any', x)
+
+let idsList: number[] = [1,2,3,5]
+console.log('idsList of type number array', idsList)
+
+let namesList : any[] = ['khaleeq',{'name':'Nouman'}]
+console.log('namesList of type any array', namesList)
+
